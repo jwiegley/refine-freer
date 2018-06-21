554891ab576c358ea970318cf2ff25a8a0a5eab6
John Wiegley <johnw@newartisans.com>
2018-06-21 14:10:44 -0700

    Add some build infrastructure

---
+++ default.nix
@@ -0,0 +1,38 @@
+{ packages ? "coqPackages_8_7"
+, rev      ? "49bdae006e66e70ad3245a463edc01b5749250d3"
+, sha256   ? "1ijsifmap47nfzg0spny94lmj66y3x3x8i6vs471bnjamka3dx8p"
+, pkgs     ? import (builtins.fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
+    inherit sha256; }) {
+    config.allowUnfree = true;
+    config.allowBroken = false;
+  }
+}:
+
+with pkgs.${packages}; pkgs.stdenv.mkDerivation rec {
+  name = "refine-freer";
+  version = "1.0";
+
+  src =
+    if pkgs.lib.inNixShell
+    then null
+    else
+    if pkgs ? coqFilterSource
+    then pkgs.coqFilterSource [] ./.
+    else ./.;
+
+  buildInputs = [
+    coq coq.ocaml coq.camlp5 coq.findlib
+    equations fiat_HEAD coq-haskell
+  ];
+  enableParallelBuilding = true;
+
+  buildPhase = "make -j$NIX_BUILD_CORES";
+  preBuild = "coq_makefile -f _CoqProject -o Makefile";
+  installFlags = "COQLIB=$(out)/lib/coq/${coq.coq-version}/";
+
+  env = pkgs.buildEnv { name = name; paths = buildInputs; };
+  passthru = {
+    compatibleCoqVersions = v: builtins.elem v [ "8.6" "8.7" "8.8" ];
+ };
+}
