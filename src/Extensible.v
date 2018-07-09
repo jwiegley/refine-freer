9436861f88bd77fa49c22ecd2147129ed4986831
John Wiegley <johnw@newartisans.com>
2018-07-09 10:17:50 -0700

    Further work

--- src/Extensible.v
+++ src/Extensible.v
@@ -27,6 +27,34 @@ Proof. inversion 1. Qed.
 (* A notation for natural transformations. *)
 Notation "f ~> g" := (forall x, f x -> g x) (at level 90).
 
+Import EqNotations.
+
+Program Definition decomp `(u : Union (t :: r) v) : t v + Union r v :=
+  match u in UnionF _ xs
+        return (t :: r)%list = xs -> t v + Union r v with
+  | UThis x => fun _ => inl (_ x)
+  | UThat x => fun _ => inr x
+  end eq_refl.
+
+Definition decomp_rev `(u : Union (r ++ [t]) v) : Union r v + t v.
+Proof.
+  induction r; simpl in u.
+    inversion u; subst.
+      exact (inr X).
+    inversion X.
+  inversion u; subst.
+    exact (inl (UThis X)).
+  destruct (IHr X).
+    exact (inl (UThat u0)).
+  exact (inr t0).
+Defined.
+
+Program Definition extract `(u : Union [t] v) : t v :=
+  match decomp u with | inl x => x | inr x => ! end.
+Next Obligation. inversion x. Qed.
+
+Definition weaken {t} `(u : Union r v) : Union (t :: r) v := UThat u.
+
 Definition Pos (t : Type -> Type) (r : list (Type -> Type)) := nat.
 
 Inductive FindElem (t : Type -> Type) : list (Type -> Type) -> Type :=
@@ -45,43 +73,49 @@ Arguments prj {t r _ v} _.
 Program Instance Member_Here (t : Type -> Type) (r : list (Type -> Type)) :
   Member t (t :: r) | 1 := {
   inj := fun _ x => UThis x;
-  prj := fun _ x => _;
+  prj := fun _ u =>
+    match decomp u with
+    | inl x => Some x
+    | inr _ => None
+    end;
   hasElem := Here _ _
 }.
-Next Obligation.
-  inversion x; subst; clear x.
-    exact (Some X).
-  exact None.
-Defined.
 
 Program Instance Member_Next (t t' : Type -> Type) (r : list (Type -> Type)) :
   Member t r -> Member t (t' :: r) | 2 := {
   inj := fun _ x => UThat (inj x);
-  prj := fun _ x => _
+  prj := fun _ u =>
+    match decomp u with
+    | inl _ => None
+    | inr u => prj u
+    end;
+  hasElem := Next _ _ _ hasElem
 }.
-Next Obligation.
-  inversion x; subst; clear x.
-    exact None.
-  exact (prj X).
-Defined.
-Next Obligation.
-  apply Next, hasElem.
-Defined.
 
-Program Definition decomp `(u : Union (t :: r) v) : t v + Union r v :=
-  match u in UnionF _ xs return (t :: r)%list = xs -> t v + Union r v with
-  | UThis x => fun _ => inl (_ x)
-  | UThat x => fun _ => inr x
-  end eq_refl.
+Fixpoint inject_last  (t : Type -> Type) (r : list (Type -> Type))
+         `(x : t a) : Union (r ++ [t]) a :=
+  match r with
+  | [] => UThis x
+  | _ :: xs => UThat (inject_last t xs x)
+  end.
 
-Program Definition extract `(u : Union [t] v) : t v :=
-  match u in UnionF _ xs return [t] = xs -> t v with
-  | UThis x => fun _ => _ x
-  | UThat x => fun H => !
-  end eq_refl.
-Next Obligation. inversion x. Qed.
+Fixpoint FindElem_last  (t : Type -> Type) (r : list (Type -> Type)) :
+  FindElem t (r ++ [t]) :=
+  match r with
+  | [] => Here _ _
+  | _ :: xs => Next _ _ _ (FindElem_last t xs)
+  end.
 
-Definition weaken {t} `(u : Union r v) : Union (t :: r) v := UThat u.
+Program Instance Member_Last (t : Type -> Type) (r : list (Type -> Type)) :
+  Member t (r ++ [t]) | 2 := {
+  inj := fun _ x => inject_last t r x;
+  prj := fun _ u =>
+    match decomp_rev u with
+    | inl _ => None
+    | inr x => Some x
+    end;
+  hasElem := FindElem_last t r
+}.
 
 Inductive Freer (f : Type -> Type) (a : Type) : Type :=
   | Pure : a -> Freer f a
@@ -159,8 +193,34 @@ Next Obligation.
 Qed.
 Next Obligation.
   unfold Freer_ap, Freer_map, comp; simpl; auto.
-  induction w, u, v; simpl; auto.
-Admitted.                       (* exercise for the reader! ;) *)
+  induction u, v, w; simpl; auto;
+  f_equal;
+  extensionality y; simpl;
+  try (specialize (H y);
+       destruct (f1 y); auto).
+  - induction (f1 y); auto.
+    f_equal.
+    extensionality z; simpl.
+    specialize (H z).
+    destruct (f3 z); auto.
+  - induction (f1 y); auto.
+    f_equal.
+    extensionality z; simpl.
+    specialize (H z).
+    destruct (f3 z); auto.
+  - induction (f1 y); auto.
+      f_equal.
+      extensionality z; simpl.
+      induction (f3 z); auto.
+      f_equal.
+      extensionality w; simpl.
+      specialize (H w).
+      destruct (f5 w); auto.
+    f_equal.
+    extensionality z; simpl.
+    specialize (H z).
+    destruct (f5 z); auto.
+Qed.
 Next Obligation.
   unfold Freer_ap, Freer_map, comp; simpl; auto.
   induction u; auto.
@@ -378,23 +438,6 @@ Polymorphic Inductive State (s : Type) : Type -> Type :=
 Arguments Get {s}.
 Arguments Put {s} _.
 
-Program Fixpoint runState {s : Type} (x : s)
-        `(f : Freer (Union (State s :: r)%list) a) :
-  Freer (Union r) a :=
-  match f with
-  | Pure v => Pure v
-  | Impure u k =>
-    match decomp u with
-    | inl f => runState x (k _)
-    | inr u => Impure u (fun y => runState x (k y))
-    end
-  end.
-Next Obligation.
-  destruct f.
-    exact x.
-  exact tt.
-Defined.
-
 Inductive Choice (A : Type) : Type :=
   | Pick : forall (P : A -> Prop), Choice A.
 
@@ -411,8 +454,6 @@ Program Instance Monad_Eff {r} : Monad (Eff r) := Freer_Monad _.
 
 Definition send `{Member eff effs} `(t : eff a) : Eff effs a :=
   Impure (inj t) Pure.
-
-Import EqNotations.
 
 Program Fixpoint Eff_size `(q : Eff effs a)
         (P : forall eff r, FindElem eff effs -> eff r -> r) : nat :=
@@ -456,13 +497,28 @@ Qed.
 
 Import ListNotations.
 
-Program Fixpoint runM `{M : Monad} `(f : Eff [m] a) : m a :=
+Program Fixpoint runM `{Monad m} `(f : Eff [m] a) : m a :=
   match f with
-  | Pure x => pure x
-  | Impure u q =>
-    let mb := extract u in
-    mb >>= (runM \o q)          (* jww (2018-06-15): precedence bug! *)
+  | Pure x     => pure x
+  | Impure u q => extract u >>= (runM \o q)
   end.
+
+(*
+Program Fixpoint ejectM {effs m a}
+        (ret : forall a, a -> m a)
+        (bind : forall a b, m a -> (a -> m b) -> m b)
+        (f : Eff (effs ++ [m]) a) : Eff effs (m a) :=
+  match f with
+  | Pure x => Pure (ret _ x)
+  | Impure u q =>
+    match decomp_rev u with
+    | inl u => Impure u (ejectM ret bind \o q)
+    | inr x => _
+    end
+  end.
+Next Obligation.
+  apply Pure.
+*)
 
 Definition Arr effs a b := a -> Eff effs b.
 Arguments Arr effs a b /.
@@ -473,22 +529,36 @@ Notation "c ↝ v" := (computes_to c v) (at level 40).
 
 Fixpoint handleRelay {eff effs a b}
          (ret : a -> Eff effs b)
-         (h : forall v, eff v -> Arr effs v b -> Eff effs b)
-         (f : Eff (eff :: effs) a) :
-  Eff effs b :=
+         (bind : forall v, eff v -> Arr effs v b -> Eff effs b)
+         (f : Eff (eff :: effs) a) : Eff effs b :=
   match f with
   | Pure x => ret x
   | Impure u q =>
-    let k := handleRelay ret h \o q in
+    let k := handleRelay ret bind \o q in
     match decomp u with
-    | inl x => h _ x k
+    | inl x => bind _ x k
     | inr u => Impure u k
     end
   end.
 
+Fixpoint handleRelayS {eff effs s a b}
+         (st : s)
+         (ret : s -> a -> Eff effs b)
+         (bind : forall v, s -> eff v -> (s -> Arr effs v b) -> Eff effs b)
+         (f : Eff (eff :: effs) a) : Eff effs b :=
+  match f with
+  | Pure x => ret st x
+  | Impure u q =>
+    let k st' := handleRelayS st' ret bind \o q in
+    match decomp u with
+    | inl x => bind _ st x k
+    | inr u => Impure u (k st)
+    end
+  end.
+
 Definition interpretWith {eff effs a}
-           (h : forall v, eff v -> Arr effs v a -> Eff effs a) :
-  Eff (eff :: effs) a -> Eff effs a := handleRelay Pure h.
+           (bind : forall v, eff v -> Arr effs v a -> Eff effs a) :
+  Eff (eff :: effs) a -> Eff effs a := handleRelay Pure bind.
 
 Definition interpret `(handler : eff ~> Eff effs) :
   Eff (eff :: effs) ~> Eff effs :=
@@ -497,21 +567,21 @@ Definition interpret `(handler : eff ~> Eff effs) :
 Fixpoint interpose' {eff effs a b}
          `{M : Member eff effs}
          (ret : a -> Eff effs b)
-         (h : forall v, eff v -> Arr effs v b -> Eff effs b)
+         (bind : forall v, eff v -> Arr effs v b -> Eff effs b)
          (f : Eff effs a) : Eff effs b :=
   match f with
   | Pure x => ret x
   | Impure u q =>
-    let k := interpose' ret h \o q in
+    let k := interpose' ret bind \o q in
     match @prj eff effs M _ u with
-    | Some x => h _ x k
+    | Some x => bind _ x k
     | None   => Impure u k
     end
   end.
 
 Definition interposeWith {eff effs a} `{Member eff effs}
-           (h : forall v, eff v -> Arr effs v a -> Eff effs a) :
-  Eff effs a -> Eff effs a := interpose' Pure h.
+           (bind : forall v, eff v -> Arr effs v a -> Eff effs a) :
+  Eff effs a -> Eff effs a := interpose' Pure bind.
 
 Definition interpose `{Member eff effs} `(handler : eff ~> Eff effs) :
   Eff effs ~> Eff effs :=
@@ -531,6 +601,16 @@ Definition State_func `(x : State s a) : s -> (s * a) :=
   | Get   => fun s => (s, s)
   | Put s => fun _ => (s, tt)
   end.
+
+Definition runState {s r a} (st : s) : Eff (State s :: r) a -> Eff r (a * s) :=
+  handleRelayS
+    st
+    (fun s x => Pure (x, s))
+    (fun t s x k =>
+       match x in State _ a' return a' = t -> _ with
+       | Get    => fun H => k s  (rew H in s)
+       | Put s' => fun H => k s' (rew H in tt)
+       end eq_refl).
 
 Definition refineState {s s' a} (AbsR : s -> s' -> Prop) :
   State s a -> State s' a -> Prop :=
@@ -605,8 +685,7 @@ Program Fixpoint raise {e} `(f : Eff effs a) : Eff (e :: effs) a :=
   | Impure u k => Impure (weaken u) (fun x => raise (k x))
   end.
 
-Local Obligation Tactic :=
-  program_simpl; try (eapply Union_empty; eauto).
+Local Obligation Tactic := program_simpl; try (eapply Union_empty; eauto).
 
 Program Fixpoint refine_fiat {s s' a} (AbsR : s -> s' -> Prop)
         (n : nat)
@@ -685,109 +764,127 @@ Inductive choose {a r} : Eff (Choice :: r) a -> Eff r a -> Prop :=
       choose (k v) (Impure u Pure) ->
       choose (Impure (UThat u) k) (Impure u Pure).
 
-Class Relates (eff : Type -> Type) := {
-  relates : forall a, eff a -> a -> Prop
+Class Relates (eff eff' : Type -> Type) := {
+  relates : forall a, eff a -> eff' a -> Prop
 }.
 
-Arguments relates {eff _ a} _ _.
+Arguments relates {eff eff' _ a} _ _.
 
-Inductive refine {effs effs' a} : Eff effs a -> Eff effs' a -> Prop :=
-  | PurePure : forall l r : a, l = r -> refine (Pure l) (Pure r)
+Inductive Identity (A : Type) : A -> Prop :=
+  | Id : forall x, Identity A x.
 
-  | LeftImpure :
-      forall x (u : Union effs x) (k : x -> Eff effs a),
-      forall `{Membership : Member eff effs}
-             (f : eff x), prj u = Some f ->
-      forall `{Relation : Relates eff} (y : x), relates f y ->
-      forall r, refine (k y) r -> refine (Impure u k) r
+Arguments Id {_} _.
 
-  | RightImpure :
-      forall x' (u' : Union effs' x') (k' : x' -> Eff effs' a),
-      forall `{Membership : Member eff' effs'}
-             (f' : eff' x'), prj u' = Some f' ->
-      forall `{Relation : Relates eff'} (y' : x'), relates f' y' ->
-      forall l, refine l (k' y') -> refine l (Impure u' k').
+(* Given an interpreter that can run any effect down to its denotation in
+   Gallina, we can interpret any action. *)
+Inductive interpP {effs state a}
+          (interpE : forall v, effs v -> v -> state -> state -> Prop) :
+          Freer effs a -> a -> state -> state -> Prop :=
+  | interpP_Impure :
+      forall {t} (u : effs t) (r : t) (k : t -> Freer effs a)
+             val pre mid post,
+        interpE _ u r pre mid ->
+        interpP interpE (k r) val mid post ->
+        interpP interpE (Impure u k) val pre post
+  | interpP_Pure : forall val pre post,
+      interpP interpE (Pure val) val pre post.
 
-Hint Constructors refine.
-
-Program Instance Choice_Relates : Relates Choice := {
-  relates := fun _ '(Pick P) => P
-}.
-
-Program Instance State_Relates {s : Type} : Relates (State s) := {
-  relates := fun _ f x => exists s, snd (State_func f s) = x
-}.
-
-Ltac find_term x :=
-  lazymatch x with
-  | UThat ?u => find_term u
-  | UThis ?t => constr:(t)
-  end.
-
-Ltac find_membership x :=
-  lazymatch x with
-  | UThat ?u =>
-    let H := find_membership u in
-    constr:(Member_Next _ _ _ H)
-  | UThis _ =>
-    constr:(Member_Here _ _)
-  end.
-
-Tactic Notation "pick_left" constr(H) :=
-  lazymatch goal with
-  | [ |- refine (Impure ?X _) _ ] =>
-    let t := find_term X in
-    eapply LeftImpure with (f:=t) (y:=H); simpl; eauto
-  end; simpl.
-
-Tactic Notation "epick_left" :=
-  lazymatch goal with
-  | [ |- refine (Impure ?X _) _ ] =>
-    let t := find_term X in
-    eapply LeftImpure with (f:=t); simpl; eauto
-  end; simpl.
-
-Tactic Notation "pick_right" constr(H') :=
-  lazymatch goal with
-  | [ |- refine _ (Impure ?Y _) ] =>
-    let t' := find_term Y in
-    eapply RightImpure with (f':=t') (y':=H'); simpl; eauto
-  end; simpl.
-
-Tactic Notation "epick_right" :=
-  lazymatch goal with
-  | [ |- refine _ (Impure ?Y _) ] =>
-    let t' := find_term Y in
-    eapply RightImpure with (f':=t'); simpl; eauto
-  end; simpl.
-
-Lemma refine_matched `{Relates t}
-      `(xu : Union (t :: effs)  a) `(xk : a -> Eff (t :: effs)  b)
-      `(yu : Union (t :: effs') a) `(yk : a -> Eff (t :: effs') b) :
-  forall x, prj (Member:=Member_Here _ _) xu = Some x ->
-  forall y, prj (Member:=Member_Here _ _) yu = Some y ->
-       x = y
-    -> (exists v, relates x v /\ relates y v /\ refine (xk v) (yk v))
-    -> refine (Impure xu xk) (Impure yu yk).
+(* This theorem present the meaning of state, interpreted as an "effect" on an
+   input state and resulting in a final value. *)
+Theorem meaning_of_State {s v} (f : State s v) (x : v) (start : s) :
+  interpP
+    (fun v (st : State s v) (x : v) (pre : s) (post : s) =>
+       match st in State _ v' return v' = v -> _ with
+       | Get   => fun H => post = pre /\ x = rew H in post
+       | Put p => fun H => post = p   /\ x = rew H in tt
+       end eq_refl)
+    (sendF f)
+    (match f in State _ v' return v' = v -> _ with
+     | Get   => fun H => start
+     | Put p => fun H => tt
+     end eq_refl)
+    start
+    (match f in State _ v' return v' = v -> _ with
+     | Get   => fun H => start
+     | Put p => fun H => p
+     end eq_refl).
 Proof.
-  intros.
-  subst.
-  repeat match goal with
-         | [ H : exists _ : _, _ |- _ ] => destruct H
-         | [ H : _ /\ _ |- _ ] => destruct H
-         end.
-  now econstructor; eauto.
+  unfold sendF.
+  induction f; simpl;
+  eapply interpP_Impure; eauto;
+  eapply interpP_Pure.
 Qed.
 
-Hint Extern 3 (prj ?X = Some _) =>
-  let t := find_term X in instantiate (1:=t); auto.
+Theorem final {a} {x : Eff [] a} {v state} {pre post : state} :
+  run x = v ->
+  interpP (fun _ u _ _ _ => False_rect _ (Union_empty _ u)) x v pre post.
+Proof.
+  simpl; intros; subst.
+  induction x; simpl.
+    now apply interpP_Pure.
+  now inversion f.
+Qed.
 
-Ltac refinement :=
-  repeat
-    lazymatch goal with
-    | [ |- refine (Impure ?X _) (Impure ?Y _) ] =>
-      eapply refine_matched; eauto
-    end.
+Definition Refines {eff eff' state state'}
+           (S : state -> state' -> Prop)
+           (P : forall v, eff v -> v -> state -> state -> Prop)
+           (Q : forall v, eff' v -> v -> state' -> state' -> Prop) : Prop :=
+  forall a (x : Freer eff a) (y : Freer eff' a) r pre post pre' post',
+    (forall v (p : eff v) (q : eff' v) (x : v) (s t : state) (s' t' : state'),
+        S s s' -> S t t' -> Q v q x s' t' -> P v p x s t)
+    -> interpP Q y r pre post
+    -> interpP P x r pre' post'.
+
+(*
+Inductive handleP {eff effs s a}
+          (ret : a -> Eff effs a -> s -> s -> Prop)
+          (bind : forall v, eff v -> Eff effs a -> s -> s -> Prop) :
+          Eff (eff :: effs) a -> Eff effs a -> s -> s -> Prop :=
+  | handleP_Impure :
+      forall {v} (u : Union (eff :: effs) v)
+             (x : v) (k : v -> Eff (eff :: effs) a)
+             rest pre mid post,
+        match decomp u with
+        | inl r => bind v r rest pre mid
+        | inr u => handleP ret bind (k x) rest mid post
+        end ->
+        handleP ret bind (Impure u k) rest pre post
+  | handleP_Pure : forall x rest pre post,
+      ret x rest pre post ->
+      handleP ret bind (Pure x) rest pre post.
+*)
+
+Definition handleAll `(f : Eff effs a)
+           (handlers : forall eff `{Member eff effs} effs' a,
+               Eff (eff :: effs') a -> Eff effs' a) : a.
+Proof.
+Abort.
+
+Inductive relate {effs a} : Eff effs a -> a -> Prop :=
+  | RelPure : forall v, relate (Pure v) v
+  | RelImpure :
+      (* An effectful action relates to a final value if there exists an
+         intermediate state that continues on to produce the value. *)
+      forall `{Member eff effs}
+             x (u : Union effs x) (k : x -> Eff effs a) i v,
+        relate (k i) v -> relate (Impure u k) v.
+
+Example relate_ex :
+  relate (send (Put 10) ;;
+          x <- send Get ;
+          y <- send (Pick (fun x => x <= 10));
+          pure (x + y)      : Eff [Choice; State (nat : Type)] nat)
+         15.
+Proof.
+  simpl.
+  repeat econstructor; eauto.
+  instantiate (1 := 10).
+  instantiate (1 := 5).
+  constructor.
+Qed.
+
+Definition refine {effs a} (old new : Eff effs a) : Prop :=
+  forall v, relate old v -> relate new v.
 
 Example refine_works :
   refine (send (Put 10) ;;
@@ -796,9 +893,17 @@ Example refine_works :
           pure (x + y)      : Eff [Choice; State (nat : Type)] nat)
 
          (y <- send (Pick (fun x => x <= 10));
-          pure y            : Eff [Choice] nat).
+          pure y            : Eff [Choice; State (nat : Type)] nat).
 Proof.
   simpl.
+  repeat intro.
+  unshelve eapply RelImpure.
+    exact v.
+  constructor.
+  unshelve econstructor.
+    exact 5.
+  econstructor.
+  repeat econstructor; eauto.
   epick_left.                   (* Put always return unit *)
     now exists 0.
   pick_left 0.                  (* Satisfy the Get request with some answer *)
