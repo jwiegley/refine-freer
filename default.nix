c1f052a96d080b66ac11920b09794268038f3804
John Wiegley <johnw@newartisans.com>
2018-07-27 16:08:00 -0700

    Update default.nix for other users and Travis

--- default.nix
+++ default.nix
@@ -3,13 +3,13 @@
 , rev      ? "d7d31fea7e7eef8ff4495e75be5dcbb37fb215d0"
 , sha256   ? "1ghb1nhgfx3r2rl501r8k0akmfjvnl9pis92if35pawsxgp115kv"
 
-, pkgs     ? (import <darwin> {}).pkgs
-# , pkgs     ? import (builtins.fetchTarball {
-#     url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
-#     inherit sha256; }) {
-#     config.allowUnfree = true;
-#     config.allowBroken = false;
-#   }
+# , pkgs     ? (import <darwin> {}).pkgs
+, pkgs     ? import (builtins.fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
+    inherit sha256; }) {
+    config.allowUnfree = true;
+    config.allowBroken = false;
+  }
 }:
 
 with pkgs.${packages}; pkgs.stdenv.mkDerivation rec {
