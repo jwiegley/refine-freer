f739b8c6695d2944c54c9cd4f0c5bb1014581d8a
Pedro Abreu <abreu223@hotmail.com>
2018-11-27 23:20:20 -0500

    First version of opt with the three rules

--- src/Refine2.v
+++ src/Refine2.v
@@ -94,15 +94,13 @@ Definition swap_spec1: Eff [State state] unit :=
 Definition swap_spec2: Eff [State state] unit :=
   st <- send Get;
   send (Put (st & {W --> st Y}));;
-  send (Put (st & {X --> st Y}));;
-  send (Put (st & {Y --> st Y}));;
   st' <- send Get;
   send (Put (st' & {X --> st' Y}));;
   pure tt.
 
 Definition swap_impl: com :=
   W ::= Y;;;
-  X ::= Y + Y + Y.
+  X ::= Y.
 
 Eval cbn in (denote_imp swap_impl).
 Eval cbn in swap_spec1.
@@ -112,79 +110,62 @@ Eval cbn in swap_spec1.
  2) put s' >>= put s =~= put s
  3) put s >>= get =~= s
 *)
-
-Program Fixpoint opt' {effs} (bget bput: bool) (s: state) `(e: Eff (State state :: effs) v): Eff (State state :: effs) v :=
+Open Scope bool_scope.
+Fixpoint opt' {effs v} (bget bput b: bool) (s: state) (e: Eff (State state :: effs) v): Eff (State state :: effs) v :=
   match e with
   | Pure x => pure x
-  | Impure u k => let default := fun bget' bput' s' x => opt' bget' bput' s' (k x) in
-                 match decomp u with
-                 | inl st =>
-                   match st with
-                   | Get => if bget then default true false s _
-                           else Impure u (fun x => default true false _ x)
-                   | Put x => if bput then default false true s _
-                             else Impure u (default false true s)
-                   end
-(*Let's assume for now that we don't know how the effects interact with each other *)
-                 | _ => Impure u (default false false s) 
-                 end
+  | Impure u k =>
+    match decomp u with
+    | inl st =>
+      match st in State _ state return state = _ -> _ with
+      | Get => fun Heq => if (bget || b) then opt' true false b s (k (rew Heq in s))
+                      else Impure u (fun x => opt' true false false s (k x))
+      | Put st' => fun Heq => if bput then opt' false true true st' (k (rew Heq in tt))
+                        else Impure u (fun x => opt' false true true st' (k x))
+      end eq_refl
+    (*Let's assume for now that we don't know how the effects interact with each other *)
+    | _ => Impure u (fun x => opt' false false false s (k x)) 
+    end
   end.
-Next Obligation.
-  exact tt.
-Defined.
 
-Definition opt {effs} := @opt' effs false false (t_empty 0).
+Definition opt {effs v} := @opt' effs v false false false (t_empty 0).
+
+Eval cbn in (opt swap_spec2).
+
+Definition swap_spec1_opt: Eff [State state] unit :=
+  (opt swap_spec1).
+
+Definition swap_spec2_opt: Eff [State state] unit :=
+  (opt swap_spec2).
+
+Definition swap_impl_opt: Eff [State state] unit :=
+  (opt (denote_imp swap_impl)).
 
 Eval cbn in ((denote_imp swap_impl)).
-Eval cbn in (opt _ (denote_imp swap_impl)).
-Eval cbn in (opt _ swap_spec2).
-
-Eval cbn in swap_spec1.
-Eval cbn in swap_spec2.
-
-Lemma swap_respects_spec2:
-  merge_impures (denote_imp swap_impl) = swap_spec2.
-Proof.
-  reflexivity.
-Qed.
-Require Import Coq.Logic.FunctionalExtensionality.
-
-Lemma swap_disrespects_spec2:
-  merge_impures (denote_imp swap_impl) <> swap_spec1.
-Proof.
-  intro.
-  inversion H.
-  apply inj_pair2 in H1.
-  eapply equal_f in H1.
-  inversion H1.
-  apply inj_pair2 in H2.
-Admitted.
-
-
-Lemma normalize_nat : forall (a b: nat),
-  ((fun x => (fun y => x + x)) a b) = (fun x => x + x) a.
-Proof.
-  intros.
-  simpl.
-  reflexivity.
-Qed.
-
-(*
-Fixpoint normalize_r `(e: Eff (State v :: effs) unit) (b: bool) (c: v)
-  : Eff (State v :: effs) unit :=
-  match e with
-  | Pure x => pure x
-  | Impure u (fun x => k) => match decomp u with
-                         | inl l => _
-                         end
-  end.
-*)
-
+(* There is a bug on put put *)
+Eval cbn in (swap_impl_opt).
+Eval cbn in (swap_spec1_opt).
+Eval cbn in (swap_spec2_opt).
+(* I need to apply this function multiple times now, until the term normalizes.
+What can I do? *)
+Eval cbn in (opt swap_spec2_opt).
+Eval cbn in (opt swap_impl_opt).
 
 Definition refines {a effs} (e1 e2: Eff effs a) := 
   forall t, e2 = t -> e1 = t.
 
-(* Next steps is to implement the monad laws for State, check sketchbook *)
+Lemma swap_impl_respects_spec2:
+  opt swap_impl_opt = opt swap_spec2_opt.
+Proof.
+  reflexivity.
+Qed.
+
+Lemma swap_impl_respects_spec1:
+  swap_impl_opt = swap_spec1_opt.
+Proof.
+  reflexivity.
+Qed.
+
 
 
 
