716ff049c43746626ec08d413e97b3eb656cbcda
Pedro Abreu <abreu223@hotmail.com>
2018-11-28 16:02:55 -0500

    Split optmizers

--- src/Refine2.v
+++ src/Refine2.v
@@ -70,8 +70,7 @@ Fixpoint denote_bexp `{Member (State state) effs}
                  pure (negb b''))
   end.
 
-Fixpoint denote_imp `{Member (State state) effs}
-         (c: com): Eff effs unit :=
+Fixpoint denote_imp (c: com): Eff [State state] unit :=
   match c with
   | SKIP => pure tt
   | CAss x ax => (a <- denote_aexp ax;
@@ -110,7 +109,82 @@ Eval cbn in swap_spec1.
  2) put s' >>= put s =~= put s
  3) put s >>= get =~= s
 *)
+Definition state_default: forall eff r, FindElem eff [State state] -> eff r -> r.
+  intros.
+  inversion X; subst.
+  inversion X0; subst.
+  refine (t_empty 0).
+  refine tt.
+  inversion X1.
+Defined.
+
+Eval compute in (Eff_size swap_spec1 state_default).
+Eval compute in (Eff_size swap_spec2 state_default).
+Eval compute in (Eff_size (denote_imp swap_impl) state_default).
+
 Open Scope bool_scope.
+
+Fixpoint opt_getget' (b: bool) (s: state) `(e: Eff [State state] v): Eff [State state]  v :=
+  match e with
+   | Pure x => pure x
+   | Impure u k =>
+     let default := fun b' s' x => opt_getget' b' s' (k x) in
+     match extract u in State _ s return s = _ -> _ with
+     | Get => fun Heq => if b then default true s (rew [id] Heq in s)
+             else Impure u (fun x => default true s x)
+     | Put x => fun _ => Impure u (default false s)
+     end eq_refl
+  end.
+Definition opt_getget {v} := @opt_getget' false (t_empty 0) v.
+
+
+Fixpoint opt_putput `(e: Eff [State state] v): Eff [State state] v :=
+   match e with
+   | Pure x => pure x
+   | Impure u k =>
+     match extract u in State _ s return s = _ -> _ with
+     | Get => fun _ => Impure u (fun x => opt_putput (k x))
+     | Put x => fun Heq => let k' := k (rew [id] Heq in tt) in
+                       match k' with
+                       | Pure t  => Impure u (fun x' => Pure t)
+                       | Impure u' k'' =>
+                         match extract u' with
+                         | Get =>
+                           Impure u (fun x' => Impure u' (fun x'' => opt_putput (k'' x'')))
+                         | Put y => Impure u' (fun x' => opt_putput (k'' x'))
+                         end
+                       end
+     end eq_refl
+   end.
+
+Fixpoint opt_putget `(e: Eff [State state] v): Eff [State state] v :=
+   match e with
+   | Pure x => pure x
+   | Impure u k =>
+     match extract u in State _ s return s = _ -> _ with
+     | Get => fun _ => Impure u (fun x => opt_putget (k x))
+     | Put st =>
+       fun Heq => Impure u (fun x => match k x with
+                               | Pure x' => Pure x'
+                               | @Impure _ _ T' u' k' =>
+                                 match extract u' in State _ s return s = T' -> _ with
+                                 | Put st' => fun _ => opt_putget (k x)
+                                 | Get => fun Heq' => opt_putget (k' (rew [id] Heq' in st))
+                                 end eq_refl
+                               end)
+                            
+     end eq_refl
+   end.
+
+Definition putputex: Eff [State state] unit :=
+  st <- send Get;
+  send (Put (st & {W --> st X}));;
+  send (Put (st & {W --> st Y; X --> st Y}));;
+  send (Put (st & {W --> st X}));;
+  pure tt.
+
+Eval cbn in (opt_putput (opt_putput putputex)).
+
 Fixpoint opt' {effs v} (bget bput b: bool) (s: state) (e: Eff (State state :: effs) v): Eff (State state :: effs) v :=
   match e with
   | Pure x => pure x
