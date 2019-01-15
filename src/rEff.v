fdb14a0deab49f4ab8f8079e4295414e028cb664
Pedro Abreu <abreu223@hotmail.com>
2019-01-15 17:52:01 -0500

    rFreer Ap and Join

--- src/rEff.v
+++ src/rEff.v
@@ -1,26 +1,51 @@
 Require Import
   Coq.Program.Program
   Coq.Program.Tactics
-  Hask.Control.Monad.
+  Hask.Control.Monad
+  Coq.Init.Datatypes.
 
 Require Import ExtLib.Data.HList.
 
-Generalizable All Variables.
+Import EqNotations.
 
-Inductive rFreer (f : list Type -> Type -> Type) (a : Type) : Type :=
+Generalizable All Variables.
+Set Printing Universes.
+
+Inductive rFreer (f : ((list Type): Type) -> Type -> Type) (a : Type) : Type :=
 | rPure : a -> rFreer f a
-| rImpure : forall x ts, f ts x -> (hlist (fun _ => rFreer f a) ts) -> (x -> rFreer f a) -> rFreer f a.
+| rImpure : forall (x: Type) (ts: (list Type): Type), f ts x -> (hlist (rFreer f) ts) -> (x -> rFreer f a) -> rFreer f a.
 
 Arguments rPure {f a} _.
 Arguments rImpure {f a x ts} _ _ _.
 
-Fixpoint rFreer_map {r} `(f : a -> b) (x : rFreer r a) : rFreer r b :=
+Program Fixpoint rFreer_fmap {r} `(f : a -> b) (x : rFreer r a) : rFreer r b :=
   match x with
   | rPure v => rPure (f v)
-  | rImpure u l k => rImpure u (hlist_map (fun _ => rFreer_map f) l) (fun x => rFreer_map f (k x))
+  | rImpure u l k => rImpure u l (fun x => rFreer_fmap f (k x))
   end.
 
 Program Instance rFreer_Functor (f : list Type -> Type -> Type) : Functor (rFreer f) := {
-  fmap := @rFreer_map _
+  fmap := @rFreer_fmap f
 }.
 
+Fixpoint rFreer_ap {r} `(f : rFreer r (a -> b)) (x : rFreer r a) : rFreer r b :=
+  match f, x with
+  | rPure f, rPure v       => rPure (f v)
+  | rPure f, rImpure u l k => rImpure u l (fun x' => rFreer_fmap f (k x'))
+  | rImpure u l k, m       => rImpure u l (fun x' => rFreer_ap (k x') m)
+  end.
+
+Program Instance rFreer_Applicative (f : list Type -> Type -> Type) : Applicative (rFreer f) := {
+  pure := fun _ => rPure;
+  ap := fun _ _ => rFreer_ap
+}.
+
+Polymorphic Fixpoint rFreer_join {r: list Type -> Type -> Type} {a} (f : rFreer r (rFreer r a)) : rFreer r a :=
+  match f with
+  | Pure v     => v
+  | Impure u l k => Impure u l (fun x => rFreer_join (k x))
+  end.
+
+Program Instance Freer_Monad (f : Type -> Type) : Monad (Freer f) := {
+  join := @Freer_join _
+}.
